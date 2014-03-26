26ea6cbc779d0aa56ea639424ebb4d2f39b90758
Callum Locke <callum.locke@gmail.com>
2014-03-26 14:52:30 +0000

    first proper commit

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-ig-furniture
-============
+# ig-furniture
 
-FT Graphics furniture
+## Install
+
+`bower install --save https://github.com/ft-interactive/ig-furniture.git`
+
+## Footer view
+
+In your HTML:
+
+```html
+<footer class="ig-furniture"></footer>
+```
+
+In your JavaScript:
+
+```javascript
+var Footer = require('ig-furniture/footer');
+
+var footer = new Footer({
+  el: $('.ig-footer')[0],
+  credits: app.data.credits,
+  footnotes: ['Some footnote string']
+});
+
+footer.render();
+```
+
+In your SCSS:
+
+```scss
+@import 'ig-furniture/footer/style';
+```
+
+Notes:
+
+- CSS classes are in BEM style, with `ig-furniture__` used as a prefix for all descendent selectors.
+  - You can also add any of these modifier classes to the container element: `ig-footer--right`, `ig-footer--no-border`, `ig-footer--single-deck`
---
+++ footer/index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var mapCredits = function (source) {
+  return source && source.name ? '<span class="ig-footer__credit">' + (source.link ? '<a href="' + source.link + '" target="_blank">' + source.name + '</a>' : source.name) + '</span>' : '';
+};
+
+var printList = function (prefix, list, suffix, separator) {
+  return (!!prefix ? '<span class="ig-footer__prefix">' + prefix + '</span>' : '') + list.map(mapCredits).join(separator || ', ') + (suffix ?  '<span class="ig-footer__suffix">' + suffix + '</span>' : '');
+};
+
+
+var Footer = function (options) {
+  this.el = options.el;
+
+  this.credits = options.credits.filter(function (row) {
+    return row && row.type === 'credit';
+  });
+
+  this.sources = options.credits.filter(function (row) {
+    return row && row.type === 'source';
+  });
+
+  if (options.footnotes) {
+    this.footnotes = options.footnotes;
+    if (!Array.isArray(this.footnotes)) {
+      this.footnotes = this.footnotes.toString().split(/[\r\n]+/);
+    }
+  } else options.footnotes = [];
+
+  this.strings = {
+    graphicType: 'Graphic'
+  };
+};
+
+Footer.prototype.template = function () {
+  var dom = '', footnotesDom = '';
+
+  if (this.sources && this.sources.length) {
+    dom += (
+      '<span class="ig-footer__sources">' +
+      printList('Source' + (this.sources.length > 1 ? 's' : '') + ': ', this.sources) +
+      '&nbsp;</span>'
+    );
+  }
+
+  if (this.credits && this.credits.length) {
+    dom += (
+      '<span class="ig-footer__credits">' +
+      printList(this.strings.graphicType + ' by ', this.credits) +
+      '</span>'
+    );
+  }
+  
+  if (this.footnotes.length) {
+    footnotesDom = '<div class="ig-footer__footnotes">' + this.footnotes.map(function(footnote) {
+      return '<p>' + footnote + '</p>';
+    }).join('') + '</div>';
+  }
+
+  return (
+    footnotesDom +
+    '<div class="ig-footer__sources-and-credits"><div>' +
+    dom +
+    '</div></div>'
+  );
+};
+
+Footer.prototype.render = function () {
+  this.el.innerHTML = this.template();
+};
+
+module.exports = Footer;
